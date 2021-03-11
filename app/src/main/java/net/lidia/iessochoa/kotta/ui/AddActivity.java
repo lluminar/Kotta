231062b58070901d0367977d97404ea703499bad
lluminar <60161182+lluminar@users.noreply.github.com>
2021-03-11 23:28:31 +0100

    Spinner e icono app

--- app/src/main/java/net/lidia/iessochoa/kotta/ui/AddActivity.java
+++ app/src/main/java/net/lidia/iessochoa/kotta/ui/AddActivity.java
@@ -2,16 +2,27 @@ package net.lidia.iessochoa.kotta.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
-
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import net.lidia.iessochoa.kotta.R;
 
 public class AddActivity extends AppCompatActivity {
 
+    private String[] categorias;
+    private AutoCompleteTextView actvCategoria;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        categorias = getResources().getStringArray(R.array.category);
 
+        actvCategoria = findViewById(R.id.fedCategoria);
+
+        //Se crean los menús para los spinners
+        ArrayAdapter<String> adaptador1;
+        AutoCompleteTextView actv;
+        adaptador1 = new ArrayAdapter<>(this,R.layout.list_item_sp, categorias);
+        actv = findViewById(R.id.fedCategoria);
+        actv.setAdapter(adaptador1);
     }
 }
