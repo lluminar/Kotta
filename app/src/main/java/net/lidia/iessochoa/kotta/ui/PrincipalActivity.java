f47e5ca62dd715d5067557c89eaf345de5f2a22c
lluminar <60161182+lluminar@users.noreply.github.com>
2021-03-17 00:16:58 +0100

    Log Out

--- app/src/main/java/net/lidia/iessochoa/kotta/ui/PrincipalActivity.java
+++ app/src/main/java/net/lidia/iessochoa/kotta/ui/PrincipalActivity.java
@@ -1,16 +1,27 @@
 package net.lidia.iessochoa.kotta.ui;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,19 +33,27 @@ import androidx.appcompat.widget.Toolbar;
 import net.lidia.iessochoa.kotta.R;
 
 public class PrincipalActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+
+    private AppBarConfiguration mAppBarConfiguration;
+
     private ImageView ivAuthorGoogle;
     private TextView tvName;
     private TextView tvEmail;
+    private TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(v -> {
+            logOut();
+        });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -61,6 +80,30 @@ public class PrincipalActivity extends AppCompatActivity {
         tvEmail.setText(currentUser.getEmail());
         if (currentUser.getPhotoUrl() != null)
             Glide.with(this).load(currentUser.getPhotoUrl()).into(ivAuthorGoogle);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void logOut() {
+        mAuth.signOut();
+        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(mainActivity);
+                    PrincipalActivity.this.finish();
+                } else
+                    Toast.makeText(getApplicationContext(), "Logout error",Toast.LENGTH_LONG).show();
+            }
+        });
+        Intent mainActivity = new Intent(this, MainActivity.class);
+        startActivity(mainActivity);
+        this.finish();
     }
 
     @Override
